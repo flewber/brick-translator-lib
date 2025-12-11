f149bbeab59856cdd332d3002830c579d7c7f611
Alec Steinkraus <flewber@gmail.com>
2025-12-11 01:33:42 -0600

    update get-translations.py to continuously poll Rebrickable API to save local copy of part name translations

--- get-translations.py
+++ get-translations.py
@@ -7,41 +7,84 @@ import csv
 
 REBRICKABLE_SLEEP_DURATION = 1.1
 
-filename = "parts.csv"  # File name
+part_filename = "parts.csv"  # File name
+translation_filename = "part_translations.csv"  # File name
 fields = []  # Column names
-rows = []    # Data rows
+part_rows = []    # Data rows
+translation_rows = ['rebrickable_part_num, non_rebrickable_part_num(s)']
+total_part_count = 0
 
-with open(filename, 'r') as csvfile:
+with open(part_filename, 'r') as csvfile:
     csvreader = csv.reader(csvfile)  # Reader object
 
     fields = next(csvreader)  # Read header
     for row in csvreader:     # Read rows
-        rows.append(row)
+        part_rows.append(row)
 
-    print("Total no. of rows: %d" % csvreader.line_num)  # Row count
+    total_part_count = csvreader.line_num
+    print("Total no. of rows: %d" % total_part_count)  # Row count
+    csvfile.close()
 
 print('Field names are: ' + ', '.join(fields))
 
 # init rebrick module for general reading
 rebrick.init(keys.YOUR_REBRICKABLE_API_KEY)
 
-print('\nFirst 5 rows are:\n')
-for row in rows[:5]:
-    for col in row:
-        print("%10s" % col, end=" ")
-    print('\n')
+# print('\nFirst 5 rows are:\n')
+# for row in part_rows[:5]:
+#     for col in row:
+#         print("%10s" % col, end=" ")
+#     print('\n')
+
+#pickup where we left off
+num_rows = 0
+try:
+    with open(translation_filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)  # Reader object
+
+        translation_fields = next(csvreader)  # Read header
+        for row in csvreader:     # Read rows
+            translation_rows.append(row)
+
+        num_rows = csvreader.line_num
+        print("Total no. of rows: %d" % num_rows)  # Row count
+        next_part_index = num_rows - 1
+    
+    
+except:
+    print("exception")
+    next_part_index = 0
+
+# print(translation_rows[next_part_index])
+# print(part_rows[next_part_index])
+
+
+
 
 # print('\nLast 5 rows are:\n')
 # for row in rows[-5:]:
-
-for row in rows[:5]:
+running_part_count = next_part_index
+for row in part_rows[next_part_index:]:
     part_num = row[0]
-    print(part_num)
+    # print(part_num)
     # get part info (including other website mappings)
     response = rebrick.lego.get_part(part_num)
-    print(json.loads(response.read()))
-    print('/n')
+    response_json = json.loads(response.read())
+    # print(response_json)
+    new_row = [part_num, response_json['external_ids']]
+    print(new_row)
+    # print('/n')
+    translation_rows.append(new_row)
+    running_part_count+=1
+    if(running_part_count % 100 == 0):
+        with open(translation_filename, 'w', newline='') as file:
+            print("wrote %d/%d part translations to: %s" % (running_part_count, total_part_count, translation_filename))
+            writer = csv.writer(file)
+            writer.writerows(translation_rows)
+            file.close()
     time.sleep(REBRICKABLE_SLEEP_DURATION)
+
+print("done")
 
 
 
